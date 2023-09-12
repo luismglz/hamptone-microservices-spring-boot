16cead99f648545d04a005ee5b17e1a3ba9d7c5c
Luis GL <lmgl887@gmail.com>
2023-09-12 16:19:28 -0600

    add: filter chain for auth

---
+++ hamptone-microservice-listing/src/main/java/com/ruisu/hamptonemicroservicelisting/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.ruisu.hamptonemicroservicelisting.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Value("${service.security.source-key-username}")
+    private String SECURE_KEY_USERNAME;
+
+    @Value("${service.security.source-key-password}")
+    private String SECURE_KEY_PASSWORD;
+
+    @Value("${service.security.source-key-username-2}")
+    private String SECURE_KEY_USERNAME_2;
+
+    @Value("${service.security.source-key-password-2}")
+    private String SECURE_KEY_PASSWORD_2;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+                AuthenticationManagerBuilder.class
+        );
+
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser(SECURE_KEY_USERNAME)
+                .password(new BCryptPasswordEncoder().encode(SECURE_KEY_PASSWORD))
+                .authorities(AuthorityUtils.commaSeparatedStringToAuthorityList("ADMIN"))
+                .and()
+                .withUser(SECURE_KEY_USERNAME_2)
+                .password(new BCryptPasswordEncoder().encode(SECURE_KEY_PASSWORD_2))
+                .authorities(AuthorityUtils.commaSeparatedStringToAuthorityList("DEV"))
+                .and()
+                .passwordEncoder(new BCryptPasswordEncoder());
+
+        http.securityMatchers((matchers) -> matchers
+                        .requestMatchers("/**"))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest()
+                        .hasRole("ADMIN")
+                ).csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
