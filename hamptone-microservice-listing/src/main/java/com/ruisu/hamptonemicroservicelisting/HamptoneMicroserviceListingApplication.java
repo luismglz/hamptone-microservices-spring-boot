5ff0f4ed213f66c6b335182df6345509bd514d2c
Luis GL <lmgl887@gmail.com>
2023-09-11 23:31:21 -0600

    add: empty project

---
+++ hamptone-microservice-listing/src/main/java/com/ruisu/hamptonemicroservicelisting/HamptoneMicroserviceListingApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.hamptonemicroservicelisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamptoneMicroserviceListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamptoneMicroserviceListingApplication.class, args);
+	}
+
+}
---
+++ hamptone-microservice-listing/src/test/java/com/ruisu/hamptonemicroservicelisting/HamptoneMicroserviceListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruisu.hamptonemicroservicelisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamptoneMicroserviceListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
