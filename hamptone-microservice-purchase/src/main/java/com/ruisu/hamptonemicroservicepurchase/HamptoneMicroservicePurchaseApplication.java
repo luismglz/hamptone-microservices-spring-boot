bf640987ec8e30b302c60a94f8a43790dcc5893f
Luis GL <lmgl887@gmail.com>
2023-09-13 14:50:20 -0600

    add: empty purchase service project

---
+++ hamptone-microservice-purchase/src/main/java/com/ruisu/hamptonemicroservicepurchase/HamptoneMicroservicePurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.hamptonemicroservicepurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamptoneMicroservicePurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamptoneMicroservicePurchaseApplication.class, args);
+	}
+
+}
