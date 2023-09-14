77ebbe2810aa613644fcef0409ad016be8fba675
Luis GL <lmgl887@gmail.com>
2023-09-13 23:16:01 -0600

    add: empty api gateway project

---
+++ hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/HamptoneApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.hamptoneapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamptoneApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamptoneApiGatewayApplication.class, args);
+	}
+
+}
