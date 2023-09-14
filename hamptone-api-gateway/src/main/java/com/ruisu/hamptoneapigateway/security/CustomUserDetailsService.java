870d1f0200ebd5d8bd2978187ee68661a64154c6
Luis GL <lmgl887@gmail.com>
2023-09-14 14:11:21 -0600

    add: user details service and granted authorities

---
+++ hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ruisu.hamptoneapigateway.security;
+
+import com.ruisu.hamptoneapigateway.model.User;
+import com.ruisu.hamptoneapigateway.service.UserService;
+import com.ruisu.hamptoneapigateway.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+
+        //format user role to authority context with prefix
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
