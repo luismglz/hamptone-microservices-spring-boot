8523a3192a2ea85fe5608fb503ab13a99958b820
Luis GL <lmgl887@gmail.com>
2023-09-14 13:24:10 -0600

    add: user service

--- hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/HamptoneApiGatewayApplication.java
+++ hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/HamptoneApiGatewayApplication.java
@@ -2,12 +2,21 @@ package com.ruisu.hamptoneapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HamptoneApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HamptoneApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HamptoneApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
---
+++ hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ruisu.hamptoneapigateway.service;
+
+import com.ruisu.hamptoneapigateway.model.Role;
+import com.ruisu.hamptoneapigateway.model.User;
+import com.ruisu.hamptoneapigateway.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional
+    @Override
+    public void changeRole(Role newRole, String username) {
+        userRepository.updateUserRole(username, newRole);
+    }
+}
