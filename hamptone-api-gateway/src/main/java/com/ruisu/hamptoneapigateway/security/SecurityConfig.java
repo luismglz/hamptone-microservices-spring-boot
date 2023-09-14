7d256f8c251f560892d4eb8a5a62eb0617c49fea
Luis GL <lmgl887@gmail.com>
2023-09-14 15:50:42 -0600

    add: security matchers in public and private paths

---
+++ hamptone-api-gateway/src/main/java/com/ruisu/hamptoneapigateway/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.ruisu.hamptoneapigateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder auth = http.getSharedObject(
+                AuthenticationManagerBuilder.class
+        );
+
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);
+
+        AuthenticationManager authenticationManager = auth.build();
+
+        http.securityMatcher("/api/auth/sign-in", "/api/auth/sign-up")
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authenticationManager(authenticationManager)
+                .sessionManagement((sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                );
+        return http.build();
+    }
+
+}
